58dfaf64883b4402d449b07464e63a6b1023fb19
Jan Misar <jan.misar@ackee.cz>
2018-09-03 09:42:56 +0200

    Use FlowCoordinator class from ACKategories.

--- ProjectTemplate/AppDelegates/MainAppDelegate.swift
+++ ProjectTemplate/AppDelegates/MainAppDelegate.swift
@@ -1,16 +1,18 @@
 import UIKit
+import ACKategories
 
 /// Use as classic app delegate - for app flow and lifecycle handling, appearance settings etc.
 final class MainAppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    // swiftlint:disable force_unwrapping
-    private lazy var appFlowController = AppFlowController(window: window!)
+    private lazy var appFlowCoordinator = AppFlowCoordinator()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        appFlowController.start()
+
+        // swiftlint:disable force_unwrapping
+        appFlowCoordinator.start(in: window!)
         return true
     }
 }
