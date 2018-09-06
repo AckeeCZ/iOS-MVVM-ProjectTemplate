9b86854f7ab878d1248179288180dfacff7e26f0
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-09-06 12:40:10 +0200

    Add doc comments for `PushNotification`

--- ProjectTemplate/Model/PushNotification.swift
+++ ProjectTemplate/Model/PushNotification.swift
@@ -1,15 +1,31 @@
 import UserNotifications
 
+/// Model object for push notification
+///
+/// When user opens a notification this object should be created
 struct PushNotification {
+    /// Payload of push notification
+    ///
+    /// When notification is received (*NOT* opened) while the app is in foreground
+    /// instance of this object should be created
     struct Payload {
 
     }
 
+    /// List of actions which can be triggered when opening a notification
+    ///
+    /// Probably just add `String` raw value.
+    /// Unluckily enum with raw value must have some cases so it is not possible to have it here empty
     enum Action {
 
     }
 
+    /// Action which triggered opening of this notification
+    ///
+    /// Is `nil` if notification was opened without any action
     let action: Action?
+    
+    /// Content data of notification
     let payload: Payload
 }
 
