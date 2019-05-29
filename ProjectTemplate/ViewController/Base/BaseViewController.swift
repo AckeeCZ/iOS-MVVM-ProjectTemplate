398f2a38b10d424907c0d440caffedbbb8181605
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-05-29 14:58:07 +0200

    Remove code that was moved to ACKategories

--- ProjectTemplate/ViewController/Base/BaseViewController.swift
+++ ProjectTemplate/ViewController/Base/BaseViewController.swift
@@ -1,33 +1,10 @@
 import UIKit
-import os.log
+import ACKategories
 
 /// Base class for all view controllers contained in app.
-class BaseViewController: UIViewController, PopupPresenting {
-
-    static var logEnabled: Bool = true
-
+class BaseViewController: Base.ViewController, PopupPresenting {
     /// Presenting modal views
     lazy var popupAnimation = PopupModalAnimation()
-
-    /// Navigation bar is shown/hidden in viewWillAppear according to this flag
-    var hasNavigationBar: Bool = true
-
-    private var firstWillAppearOccured = false
-    private var firstDidAppearOccured = false
-
-    // MARK: Initializers
-
-    init() {
-        super.init(nibName: nil, bundle: nil)
-
-        if BaseViewController.logEnabled {
-            os_log("📱 👶 %@", log: Logger.lifecycleLog(), type: .info, self)
-        }
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
 
     // MARK: View life cycle
 
@@ -35,42 +12,6 @@ class BaseViewController: UIViewController, PopupPresenting {
         super.loadView()
 
         view.backgroundColor = .white
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if !firstWillAppearOccured {
-            viewWillFirstAppear(animated)
-            firstWillAppearOccured = true
-        }
-
-        navigationController?.setNavigationBarHidden(!hasNavigationBar, animated: animated)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        if !firstDidAppearOccured {
-            viewDidFirstAppear(animated)
-            firstDidAppearOccured = true
-        }
-    }
-
-    /// Method is called when `viewWillAppear(_:)` is called for the first time
-    func viewWillFirstAppear(_ animated: Bool) {
-
-    }
-
-    /// Method is called when `viewDidAppear(_:)` is called for the first time
-    func viewDidFirstAppear(_ animated: Bool) {
-
-    }
-
-    deinit {
-        if BaseViewController.logEnabled {
-            os_log("📱 ⚰️ %@", log: Logger.lifecycleLog(), type: .info, self)
-        }
     }
 }
 
