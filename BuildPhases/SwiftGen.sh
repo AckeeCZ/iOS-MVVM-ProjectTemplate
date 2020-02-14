f48a9a8c9556c5ff18ad3b6a043b86fad9b17ebf
Marek Fořt <marek.fort@ackee.cz>
2020-02-14 14:53:22 +0100

    🔧 Use TARGET_NAME instead of ACK_PRODUCT_NAME.

--- BuildPhases/SwiftGen.sh
+++ BuildPhases/SwiftGen.sh
@@ -2,14 +2,14 @@
 
 export PATH="${PODS_ROOT}/SwiftGen/bin:$PATH"
 
-LOC_SRC_FILE="${PROJECT_DIR}/${ACK_PRODUCT_NAME}/Resources/en.lproj/Localizable.strings"
-LOC_DST_FILE="${PROJECT_DIR}/${ACK_PRODUCT_NAME}/Model/Generated/LocalizedStrings.swift"
+LOC_SRC_FILE="${PROJECT_DIR}/${TARGET_NAME}/Resources/en.lproj/Localizable.strings"
+LOC_DST_FILE="${PROJECT_DIR}/${TARGET_NAME}/Model/Generated/LocalizedStrings.swift"
 
-SHARED_IMG_SRC_FILE="${PROJECT_DIR}/${ACK_PRODUCT_NAME}/Resources/Images.xcassets"
-ASSETS_DST_FILE="${PROJECT_DIR}/${ACK_PRODUCT_NAME}/Model/Generated/Assets.swift"
+SHARED_IMG_SRC_FILE="${PROJECT_DIR}/${TARGET_NAME}/Resources/Images.xcassets"
+ASSETS_DST_FILE="${PROJECT_DIR}/${TARGET_NAME}/Model/Generated/Assets.swift"
 
 PLIST_SRC_FILE="${ACK_ENVIRONMENT_DIR}/Current/environment.plist"
-PLIST_DST_FILE="${PROJECT_DIR}/${ACK_PRODUCT_NAME}/Model/Generated/Environment.swift"
+PLIST_DST_FILE="${PROJECT_DIR}/${TARGET_NAME}/Model/Generated/Environment.swift"
 
 echo "[SwiftGen] Generating strings"
 swiftgen strings -t structured-swift4 "$LOC_SRC_FILE" -o "${LOC_DST_FILE}"
