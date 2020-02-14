fca8cafd40923d560a92f16f82214d43775a7f57
Rostislav Babáček <babacros@fit.cvut.cz>
2020-02-14 08:32:31 +0100

    🔥 remove useless return from testing app delegate

--- Tests/Unit/TestingAppDelegate.swift
+++ Tests/Unit/TestingAppDelegate.swift
@@ -4,6 +4,6 @@ final class TestingAppDelegate: NSObject, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        return true
+        true
     }
 }
