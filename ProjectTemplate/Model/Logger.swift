2c77decec483be3fa2134c8e6880176da4492145
Rostislav Babáček <babacros@fit.cvut.cz>
2020-02-14 08:24:58 +0100

    🔥 remove useless returns from logger

--- ProjectTemplate/Model/Logger.swift
+++ ProjectTemplate/Model/Logger.swift
@@ -11,32 +11,32 @@ import os.log
 /// Contains pre-defined OSLog categories
 struct LoggerCategory {
     /// Generic app logging category
-    static var app: String { return "App" }
+    static var app: String { "App" }
     /// Networking logging category
-    static var networking: String { return "Networking" }
+    static var networking: String { "Networking" }
     /// Lifecycle logging category
-    static var lifecycle: String { return "Lifecycle" }
+    static var lifecycle: String { "Lifecycle" }
 }
 
 /// Wrapper class for os_log function
 struct Logger {
     /// Create OSLog with subsystem and category
     static func osLog(subsystem: String = Bundle.main.bundleIdentifier ?? "-", category: String) -> OSLog {
-        return OSLog(subsystem: subsystem, category: category)
+        OSLog(subsystem: subsystem, category: category)
     }
 
     /// Create app log
     static func appLog() -> OSLog {
-        return OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.app)
+        OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.app)
     }
 
     /// Create networking log
     static func networkingLog() -> OSLog {
-        return OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.networking)
+        OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.networking)
     }
 
     /// Create lifecycle log
     static func lifecycleLog() -> OSLog {
-        return OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.lifecycle)
+        OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: LoggerCategory.lifecycle)
     }
 }
