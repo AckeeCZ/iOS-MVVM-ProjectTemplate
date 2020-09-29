b60da5538be372174cb41c1132b213448abca374
Jakub Olejnik <jakub.olejnik@ackee.cz>
2020-09-29 14:30:00 +0200

    🔥 Remove extra app delegates

--- ProjectTemplate/AppDelegate.swift
+++ ProjectTemplate/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import ACKategories
+import FirebaseCore
 
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
@@ -7,6 +8,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     private lazy var appFlowCoordinator = AppFlowCoordinator()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // TODO: Uncomment this line when your Google plists are ready!
+        // FirebaseApp.configure()
+
         // Clear launch screen cache on app launch (debug and beta configurations only)
         #if DEBUG || ADHOC
         application.clearLaunchScreenCache()
