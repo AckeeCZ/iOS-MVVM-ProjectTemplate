c5a654cb4d40f63a4e6edce01b589636d0e19362
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-07-11 15:58:12 +0200

    ✨ Add HasTesting extensions to Tuist plugin

---
+++ TuistPlugin/ProjectDescriptionHelpers/HasTesting.swift
@@ -0,0 +1,26 @@
+import Foundation
+import ProjectDescription
+
+public extension SourceFileGlob {
+    /// Checks if there is _Testing_ folder at given path and if used configuration is debug and returns its glob if so.
+    /// - Parameter path: Path where existence of _Testing_ folder should be checked
+    /// - Parameter isDebug: If current configuration is not debug, then testing will not be returned
+    /// - Returns: Glob four testing folder if any and if configuration is debug, nil otherwise
+    static func testing(
+        at path: String,
+        isDebug: Bool = Configuration.current.isDebug
+    ) -> SourceFileGlob? {
+        guard !isDebug else { return nil }
+        
+        var isDirectory: ObjCBool = false
+        let testingPath = path + "/Testing"
+        
+        let exists = FileManager.default.fileExists(atPath: testingPath, isDirectory: &isDirectory)
+        
+        guard exists, isDirectory.boolValue else {
+            return nil
+        }
+        
+        return "\(testingPath)/**"
+    }
+}
