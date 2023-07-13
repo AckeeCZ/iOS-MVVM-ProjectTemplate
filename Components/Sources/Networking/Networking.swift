99dd9007924f333888ef86de95eb75b192613ba8
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-07-13 16:16:45 +0200

    ♻️ Get rid of custom Networking object, use URLSession instead

--- Components/Sources/Networking/Networking.swift
+++ Components/Sources/Networking/Networking.swift
@@ -8,15 +8,9 @@ public protocol HasNetwork {
     var network: Networking { get }
 }
 
-public final class Network: Networking {
-    private let session: URLSession
-    
-    public init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
+extension URLSession: Networking {
     public func request(_ request: URLRequest) async throws -> HTTPResponse {
-        let (data, response) = try await session.data(for: request)
+        let (data, response) = try await data(for: request)
         
         return .init(
             request: request,
