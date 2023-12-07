2bd44ee23453411a423df1911ce47ede4207b578
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-12-07 14:54:27 +0100

    ⬆️ Update plugin

--- ProjectTemplate/Tuist/Config.swift
+++ ProjectTemplate/Tuist/Config.swift
@@ -4,8 +4,7 @@ let config = Config(
     plugins: [
         .git(
             url: "https://github.com/AckeeCZ/AckeeTemplate-TuistPlugin",
-//            tag: "0.1.3"
-            sha: "2f49af1dd17d25b29d9151765a456b94d233a9c8"
+            tag: "0.2.0"
         )
     ]
 )
